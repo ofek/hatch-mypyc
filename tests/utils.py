8d75fd31465e403c70852f89834debd0601968fe
Ofek Lev <ofekmeister@gmail.com>
2022-07-24 10:23:14 -0400

    update (#17)

--- tests/utils.py
+++ tests/utils.py
@@ -6,8 +6,9 @@ import sys
 
 
 def build_project(*args):
-    process = subprocess.run(
-        [sys.executable, '-m', 'hatchling', 'build', *args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT
-    )
+    if not args:
+        args = ['-w']
+
+    process = subprocess.run([sys.executable, '-m', 'build', *args], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if process.returncode:  # no cov
         raise Exception(process.stdout.decode('utf-8'))
